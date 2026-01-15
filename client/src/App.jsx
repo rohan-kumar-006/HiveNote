82eb79ef57f96ee8dba3f8ac5a4ddfef8676c2d4
[Rohan Kumar] <rohanchauhan3845@gmail.com>
2026-01-15 14:22:18 +0530

    svg import issue resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Login from "../src/pages/Login.jsx"
 import './App.css'
 import Register from './pages/Register.jsx'
